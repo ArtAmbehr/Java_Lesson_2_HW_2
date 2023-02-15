4b2c2ab16f6b48e82464657cad21f2ae9b0de1bd
Art <art-75@yandex.ru>
2023-02-15 21:08:59 +0300

    positivPow

--- Task_2.java
+++ Task_2.java
@@ -12,3 +12,48 @@
 
 // нельзя использовать math.pow
 
+import java.io.File; // импортируем класс для работы с файлами
+import java.io.FileNotFoundException; // импортируем класс обработки ошибки отсутствия файла
+import java.util.Scanner; // импортируем класс для чтения информации из консоли, файла и т.п.
+import java.io.PrintWriter; // Импортируем класс записи в файл
+
+public class Task_2 {
+    public static void main(String[] args) throws FileNotFoundException { 
+        int[] array = readFile();
+        int a = array[0];
+        int b = array[1];
+        if(a == 0 && b == 0) writeFile("Не определено");
+        else if(b < 0) writeFile(negativPow(a, b));
+        else writeFile(positivPow(a, b));
+    }
+
+    public static int[] readFile() throws FileNotFoundException {
+        File file = new File("input.txt");
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        int[] intArray = new int[2];
+        String[] stringArray = line.split(" ");
+        if(stringArray[0] == "a"){
+            intArray[0] = Integer.parseInt(stringArray[1]);
+            stringArray = scanner.nextLine().split(" ");
+            intArray[1] = Integer.parseInt(stringArray[1]);
+        }
+        else {
+            intArray[1] = Integer.parseInt(stringArray[1]);
+            stringArray = scanner.nextLine().split(" ");
+            intArray[0] = Integer.parseInt(stringArray[1]);
+        }
+        scanner.close();
+        return intArray;
+    }
+
+    public static String positivPow(int a, int b) {
+        int temp = 1;
+        String result = "";
+        for(int i = 0; i < b; i++){
+            temp *= a;
+        }
+        result = String.valueOf(temp);
+        return result;
+    }
+
